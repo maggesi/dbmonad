28f066ca9a3720deaa9a9d05bdee73cdf40279a7
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2021-04-30 16:12:21 +0200

    Move attempts for explicit substitution in a separate folder. Also delete old latex files from 2017.

--- Docs/Notes/copyfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MODTIME=$(stat -f '%Sm' -t '%F-%R' dbmonad.tex)
-DEST="$HOME/Dropbox/Devel/ahmm/Notes/Substoids"
-
-cp -a dbmonad.tex "$DEST/dbmonad-$MODTIME".tex
-cp -a dbmonad.pdf "$DEST/dbmonad-$MODTIME".pdf
